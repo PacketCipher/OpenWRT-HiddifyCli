2be6e19789b8623954a8959c01234e264f6a0544
Foxy <firebot1020@gmail.com>
2024-06-11 01:39:13 +0330

    Fix hiddify_watch and increase cron time to every 4h

--- hiddify_watchdog.sh
+++ hiddify_watchdog.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 SERVICE_DIR="/tmp/usr/bin"
-RUN_CMD="./HiddifyCli run --config "https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/xray/normal/mix#MIX" --hiddify /root/hiddify-conf.json"
+CONF_FILE="/root/hiddify-conf.json"
+RUN_CMD="./HiddifyCli run --config "https://raw.githubusercontent.com/PacketCipher/TVC/main/subscriptions/xray/normal/mix" --hiddify $CONF_FILE"
+WEB_SECRET=$(jq -r '.["web-secret"]' "$CONF_FILE")
 
 while true; do
     if ! pgrep -fl HiddifyCli &>/dev/null; then
         echo "HiddifyCli is not running. Restarting..."
         (cd "$SERVICE_DIR" && nohup $RUN_CMD > /dev/null 2>&1 &)
-        sleep 300
+        sleep 240
     else
         echo "Checking HiddifyCli response..."
         
@@ -16,10 +18,10 @@ while true; do
 
         if [ "$HTTP_RESPONSE_PROXY" -ne 204 ]; then
             if [ "$HTTP_RESPONSE_DIRECT" -eq 204 ]; then
-                echo "Proxy is not responding. Restarting HiddifyCli..."
-                killall HiddifyCli
-                (cd "$SERVICE_DIR" && nohup $RUN_CMD > /dev/null 2>&1 &)
-                sleep 300
+                echo "Proxy is not responding. Making HTTP call to switch group..."
+                curl -X GET "http://127.0.0.1:6756/group/select/delay?url=http://cp.cloudflare.com&timeout=2000" \
+                    -H "Authorization: Bearer $WEB_SECRET" &
+                sleep 240
             else
                 echo "Both proxy and direct checks are not returning 204 (Internet Issue). No action taken."
             fi
@@ -27,5 +29,5 @@ while true; do
             echo "HiddifyCli is running and proxy is responsive."
         fi
     fi
-    sleep 60
+    sleep 5
 done
