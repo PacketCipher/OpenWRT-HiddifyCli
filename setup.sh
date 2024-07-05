82f85391b26f1a54026234e180bcd57f59f8efad
Foxy <firebot1020@gmail.com>
2024-07-05 18:14:38 +0330

    Fix Setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ opkg install coreutils-nohup curl wget jq
 
 # Copy configuration and watchdog script, overwriting if they exist
 cp -f hiddify-conf.json /root/hiddify-conf.json
+cp -f hiddify-openvpn-conf.json /root/hiddify-openvpn-conf.json
 cp -f hiddify_watchdog.sh /root/hiddify_watchdog.sh
 cp -f hiddify_openvpn_watchdog.sh /root/hiddify_openvpn_watchdog.sh
 
